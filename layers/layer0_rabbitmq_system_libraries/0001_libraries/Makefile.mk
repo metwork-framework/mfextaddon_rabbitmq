7ee042c7b7ef1d31a3aae97bef717daaab2aa8fe
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-03-26 10:22:55 +0100

    build: fix details

--- layers/layer0_rabbitmq_system_libraries/0001_libraries/Makefile.mk
+++ layers/layer0_rabbitmq_system_libraries/0001_libraries/Makefile.mk
@@ -3,6 +3,11 @@ include $(MFEXT_HOME)/share/simple_layer.mk
 
 ifeq ($(shell cat /etc/redhat-release |grep -c "CentOS release 6"), 1)
 
+#erlang (of which we are using binaries without building it by ourselves) links
+#with libcrypto.so.10. As we are not building it, we can't tell erlang to use 
+#libcrypto.45.so provided by core@mfext. But libcrypto.so.10 is not available on
+#some linux distributions such as Debian. So we need to copy it from CentOS6.
+
 LIB_LIST = $(shell cat libraries.txt)
 $(shell mkdir $(PREFIX)/lib)
 
--- layers/layer0_rabbitmq_system_libraries/layer_custom.md
+++ layers/layer0_rabbitmq_system_libraries/layer_custom.md
@@ -2,6 +2,6 @@
 
 {% block overview %}
 
-The `scientific_system_libraries` layer contains a few copies (at build) of centos6 system libraries needed as runtime dependencies but that we don't want to build from sources
+The `rabbitmq_system_libraries` layer contains a few copies (at build) of centos6 system libraries needed as runtime dependencies but that we don't want to build from sources
 
 {% endblock %}
