73de90e591c226427a88310d376ae96ad8155188
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-04-06 08:54:34 +0000

    build: better adaptation to Metwork context

--- layers/layer1_rabbitmq/0200_rabbitmq/Makefile.mk
+++ layers/layer1_rabbitmq/0200_rabbitmq/Makefile.mk
@@ -17,5 +17,8 @@ $(PREFIX)/lib/rabbitmq/lib/$(NAME)-$(VERSION)/sbin/rabbitmq-server:
 	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard download uncompress
 	find build/usr -type f | xargs sed -i 's/\/usr\/lib\//$$MFEXT_HOME\/opt\/rabbitmq\/lib\//'
 	find build/usr -type f | xargs sed -i 's/\/usr\/sbin\//$$MFEXT_HOME\/opt\/rabbitmq\/bin\//'
+	find build/usr -type f | xargs sed -i 's/$${SYS_PREFIX}\/etc\//$$MFMODULE_RUNTIME_HOME\/var\//'
+	find build/usr -type f | xargs sed -i 's/$${SYS_PREFIX}\/var\/lib\//$$MFMODULE_RUNTIME_HOME\/var\//'
+	find build/usr -type f | xargs sed -i 's/$${SYS_PREFIX}\/var\/log\/rabbitmq/$$MFMODULE_RUNTIME_HOME/'
 	cd build/usr && rm -rf sbin
 	cd build/usr && cp -R * $(PREFIX)
